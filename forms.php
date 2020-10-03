4af21897a58dcee7c498766d55f560b6bc198fe5
pushpalathasajjala <70595014+pushpalathasajjala@users.noreply.github.com>
2020-10-03 23:31:15 +0530

    Add files via upload

---
+++ forms.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>question and answers</title>
+</head>
+<body>
+<h1>QUIZ!</h1>
+<form action="post.php" method="post">
+<h3>first question</h3>
+<p><label for="cap">what is the capital of afghanisthan?</label></p>
+
+<input type="text" name ="capital" id="cap" required="required">
+<button type="submit" value="go"> submit</button>
+
+
+
+
+
+</form>
+</body>
+</html>
